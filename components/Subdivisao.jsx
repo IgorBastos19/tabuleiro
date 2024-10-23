e602416ea1f15bc06cc7f58462ca180cc6320cc5
Igor Bastos <114706936+IgorBastos19@users.noreply.github.com>
2024-10-23 17:55:34 -0300

    tabuleiro

---
+++ components/Subdivisao.jsx
@@ -0,0 +1,12 @@
+import styles from "../styles/Subdivisao.module.css";
+
+export default function Subdivisao(props) {
+  return (
+    <div
+      style={{
+        backgroundColor: props.preto ? "black" : "white",
+      }}
+      className={styles.subdivisao}
+    ></div>
+  );
+}
---
+++ components/Tabuleiro.jsx
@@ -0,0 +1,16 @@
+import Linha from "./Linha";
+export function Tabuleiro() {
+  return (
+    <div>
+      <Linha />
+      <Linha preto />
+      <Linha />
+      <Linha preto />
+      <Linha />
+      <Linha preto />
+      <Linha />
+      <Linha preto />
+      <Linha />
+    </div>
+  );
+}
